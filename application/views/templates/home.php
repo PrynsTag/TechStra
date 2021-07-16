da351cdf46da0c406b53d5e8f62cd3935ad3ce02
CWGGa <201812027@fit.edu.ph>
2021-07-16 12:46:58 +0800

    Remove index.php from config, added designs from head_nav and footer, and added home view

--- application/views/templates/home.php
+++ application/views/templates/home.php
@@ -8,7 +8,9 @@
     <title><?= $header_title ?></title>
 
     <!-- Bootstrap 5 -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"> -->
+
+    <link rel="stylesheet" href="https://bootswatch.com/5/lux/bootstrap.min.css">
 </head>
 
 <body>
