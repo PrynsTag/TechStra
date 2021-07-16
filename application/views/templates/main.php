b44c713b2eb22ec1aaa69e701ac6a7bfae777f02
CWGGa <201812027@fit.edu.ph>
2021-07-16 11:18:32 +0800

    Fix load view bug

--- application/views/templates/main.php
+++ application/views/templates/main.php
@@ -13,7 +13,7 @@
 
 <body>
 
-	<?= $this->load->view($main_view); ?>
+	<?php $this->load->view($main_view); ?>
 
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
 	</script>
