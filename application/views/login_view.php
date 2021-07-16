ce48ddd6f9dd923ab6277efa1265271ecf9e6220
CWGGa <201812027@fit.edu.ph>
2021-07-16 11:33:18 +0800

    Added user login and added user model

--- application/views/login_view.php
+++ application/views/login_view.php
@@ -1,14 +1,12 @@
 <?php echo form_open('user_login'); ?>
 <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Email address</label>
-    <input type="text" class="form-control">
+    <label for="username" class="form-label">Username</label>
     <?= form_input($input_username); ?>
-    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
 </div>
 <div class="mb-3">
-    <label for="exampleInputPassword1" class="form-label">Password</label>
-    <input type="password" class="form-control">
+    <label for="password" class="form-label">Password</label>
     <?= form_input($input_password); ?>
 </div>
-<button type="submit" class="btn btn-primary">Submit</button>
+<button type="submit" class="btn btn-primary">Login</button>
 <?php echo form_close(); ?>
+<p>Don't have an account yet? <a href="register">Register</a></p>
