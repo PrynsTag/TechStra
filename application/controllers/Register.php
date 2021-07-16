46e03dbc51b970b34e92215efd0f1d9350d83436
PrynsTag <201812311@fit.edu.ph>
2021-07-16 12:22:20 +0800

    Added registration functionality

---
+++ application/controllers/Register.php
@@ -0,0 +1,119 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+include ".config";
+
+class Register extends CI_Controller
+{
+	public function index($data, $page = "templates/main")
+	{
+		$this->load->view($page, $data);
+	}
+
+	public function register_user()
+	{
+		$data = [
+			"header_title" => "Sign Up",
+			"main_view" => "register_view"
+		];
+		$this->index($data);
+	}
+
+	public function validation()
+	{
+		global $gmailUsername;
+		global $gmailPassword;
+
+		$this->form_validation->set_rules("firstname", "First Name", "required|trim");
+		$this->form_validation->set_rules("lastname", "Last Name", "required|trim");
+		$this->form_validation->set_rules("username", "Username", "required|trim|is_unique[user.user_username]");
+		$this->form_validation->set_rules("password", "Password", "required|trim");
+		$this->form_validation->set_rules("confirm_password", "Confirm Password", "required|trim|matches[password]");
+		$this->form_validation->set_rules("email", "Email Address", "required|trim|valid_email|is_unique[user.user_email]");
+
+		if ($this->form_validation->run()) {
+			$verification_key = mt_rand(100000, 999999);
+//            $encrypted_password = $this->encrypt->encode($this->input->post("password")); if password needs to encrypted
+			$data = array(
+				"user_firstname" => $this->input->post("firstname"),
+				"user_lastname" => $this->input->post("lastname"),
+				"user_username" => $this->input->post("username"),
+				"user_password" => $this->input->post("password"),
+				"user_email" => $this->input->post("email"),
+				"user_code" => $verification_key
+			);
+
+			$this->session->set_tempdata($data, NULL, 300);
+
+			$id = $this->register_model->insert_user($data);
+
+			if ($id > 0) {
+				$subject = "Please verify your account!";
+				$message = "<p>Hi " . $this->input->post('username') . "</p>" . "
+                         <p>Click the link to verify your email address: " . "
+                         <a href=" . base_url() . "register/verify_email/" . $verification_key . ">Link</a></p>";
+
+				$config = array(
+					"protocol" => "smtp",
+					"smtp_host" => "smtp.gmail.com",
+					"smtp_port" => 587,
+					"_smtp_auth" => TRUE,
+					"smtp_user" => $gmailUsername,
+					"smtp_crypto" => "tls",
+					"smtp_pass" => $gmailPassword,
+					"send_multipart" => FALSE,
+					"mailtype" => "html",
+					"charset" => "utf-8",
+					"wordwrap" => TRUE,
+				);
+
+				$this->email->initialize($config);
+				$this->email->set_newline("\r\n");
+				$this->email->from($gmailUsername);
+				$this->email->to($this->input->post("email"));
+				$this->email->subject($subject);
+				$this->email->message($message);
+
+				if ($this->email->send()) {
+					$this->session->set_tempdata(array("register_message" => "Check your email to verify your account!"), NULL, 1);
+					redirect("register/register_user");
+				} else {
+					show_error($this->email->print_debugger());
+				}
+			} else {
+				$this->session->set_tempdata(array("register_message" => "Account not added."), NULL, 1);
+				redirect("register/register_user");
+			}
+		} else {
+			$this->index(array(
+				"validation_error" => $this->session->set_tempdata(array("register_message" => validation_errors()), NULL, 1),
+				"header_title" => "Validation Error",
+				"main_view" => "register_view"
+			));
+		}
+	}
+
+	public function verify_email()
+	{
+		$uri_code = $this->uri->segment(3);
+
+		$result = $this->register_model->get_code($uri_code);
+		$db_code = $result->user_code;
+		$updated = False;
+
+		if ($uri_code === $db_code) {
+			$data = array(
+				'user_code' => '',
+				'user_verification' => 1
+			);
+
+			$updated = $this->register_model->verify_user($data, $db_code);
+		}
+
+		$this->index(array(
+			"email" => $result->user_email,
+			"updated" => $updated,
+			"main_view" => "register_view",
+			"header_title" => "Login - Beta Juniors",
+		));
+	}
+}
---
+++ application/views/register_view.php
@@ -0,0 +1,52 @@
+<main class="form-signin text-center">
+	<?= form_open_multipart("register/validation") ?>
+	<img class="mb-4 align-center" src="<?= base_url("assets/images/astra-tech.jpg") ?>" alt="Astratech" width="72"
+			 height="57">
+	<h1 class="h3 mb-3 fw-normal">Please sign up</h1>
+
+	<?php
+	if ($this->session->tempdata("register_message")) {
+		echo '<div class="alert alert-success">' . $this->session->tempdata("register_message") . '</div>';
+	}
+	?>
+
+	<div class="form-floating mb-3">
+		<input class="form-control" id="floatingInput" name="firstname" placeholder="Juan" type="text">
+		<label for="floatingInput">First Name</label>
+	</div>
+
+	<div class="form-floating mb-3">
+		<input class="form-control" id="floatingInput" name="lastname" placeholder="Dela Cruz" type="text">
+		<label for="floatingInput">Last Name</label>
+	</div>
+
+	<div class="form-floating mb-3">
+		<input class="form-control" id="floatingInput" name="email" placeholder="juan_delacruz@gmail.com" type="email">
+		<label for="floatingInput">Email Address</label>
+	</div>
+
+	<div class="form-floating mb-3">
+		<input class="form-control" id="floatingInput" name="username" placeholder="JuanDelaCruz" type="text">
+		<label for="floatingInput">Username</label>
+	</div>
+
+	<div class="form-floating">
+		<input class="form-control" id="floatingPassword" name="password" placeholder="Password" type="password">
+		<label for="floatingPassword">Password</label>
+	</div>
+
+	<div class="form-floating">
+		<input class="form-control" id="floatingPassword" name="confirm_password" placeholder="Confirm Password"
+					 type="password">
+		<label for="floatingPassword">Confirm Password</label>
+	</div>
+
+	<div class="checkbox mb-3">
+		<label>
+			<input type="checkbox" value="remember-me"> Remember me
+		</label>
+	</div>
+	<button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+	<p class="mt-5 mb-3 text-muted">Astratech &copy; 2017–2021</p>
+	</form>
+</main>
