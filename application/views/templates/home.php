b72e6f20fd38bef5cabad88e92da6325f145f133
PrynsTag <201812311@fit.edu.ph>
2021-07-16 19:04:09 +0800

    Removed duplicate bootstrap

--- application/views/templates/home.php
+++ application/views/templates/home.php
@@ -8,11 +8,8 @@
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title><?= $header_title ?></title>
 
-	<!-- Bootstrap -->
+	<!-- Bootstrap 5 -->
 	<link rel="stylesheet" href="https://bootswatch.com/5/lux/bootstrap.min.css">
-	s
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-		  integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
 	<!-- Custom CSS -->
 	<link rel="stylesheet" href="<?= base_url("assets/css/custom.css") ?>">
