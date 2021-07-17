454d38086ad6cd5823e524d3492a727007c716f7
PrynsTag <201812311@fit.edu.ph>
2021-07-18 05:37:47 +0800

    Changed home, posts, profile, and logout links

--- application/views/fragments/head_nav.php
+++ application/views/fragments/head_nav.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
 	<div class="container-fluid">
-		<a class="navbar-brand" href="#">Navbar</a>
+		<a class="navbar-brand" href="<?= base_url("home") ?>">Navbar</a>
 		<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
 		</button>
@@ -8,26 +8,20 @@
 		<div class="collapse navbar-collapse" id="navbarColor01">
 			<ul class="navbar-nav me-auto">
 				<li class="nav-item">
-					<a class="nav-link <?= $this->uri->segment(1) == 'home' ? 'active' : ''; ?>" href="<?= base_url('home'); ?>">Home</a>
+					<a class="nav-link <?= $this->uri->segment(1) === 'home' ? 'active' : '' ?>" href="<?= base_url('home'); ?>">Home</a>
 				</li>
 				<li class="nav-item ">
-					<a class="nav-link <?= $this->uri->segment(1) == 'posts' ? 'active' : ''; ?>" href="posts">Posts</a>
-				</li>
-				<li class="nav-item">
-					<a class="nav-link <?= $this->uri->segment(1) == 'settings' ? 'active' : ''; ?>" href="#">Settings</a>
-				</li>
-				<li class="nav-item">
-					<a class="nav-link <?= $this->uri->segment(1) == 'logout' ? 'active' : ''; ?>" href="#">Logout</a>
+					<a class="nav-link <?= $this->uri->segment(1) === 'posts' ? 'active' : '' ?>" href="posts">Posts</a>
 				</li>
 				<li class="nav-item dropdown">
 					<a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
 						<img class="rounded-circle" src="<?= base_url("assets/images/astra-tech.jpg") ?>" alt="Profile-Image" height="42" width="42">
 					</a>
 					<div class="dropdown-menu">
-						<a class="dropdown-item" href="<?= base_url("profile") ?>">Profile</a>
+						<a class="dropdown-item" href="<?= base_url("profile/user") ?>">Profile</a>
 						<a class="dropdown-item" href="#">Setting</a>
 						<div class="dropdown-divider"></div>
-						<a class="dropdown-item" href="login/user_logout">Logout</a>
+						<a class="dropdown-item" href="<?= base_url("login/user_logout") ?>">Logout</a>
 					</div>
 				</li>
 			</ul>
