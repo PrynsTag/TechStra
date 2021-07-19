aa1ed744e03dc638448721fef3732d5b40f25090
CWGGa <201812027@fit.edu.ph>
2021-07-19 18:03:24 +0800

    Post page new design

--- application/views/posts_view.php
+++ application/views/posts_view.php
@@ -5,25 +5,27 @@
 
 <!--Body-->
 <div class="container-fluid" id="cardContainer">
-    <div class="headingContainer">
-        <h1>My Collection</h1>
+    <div class="headingContainer pt-5">
+        <h1 class="text-center home-title">My Posts</h1>
     </div>
     <div class="bodyContainer">
         <div class="contentContainer">
-            <ul class="nav justify-content-end collectionCard additemnavbar">
-                <li class="nav-item">
-                    <a class="nav-link" href="<?= base_url() . "posts/add" ?>">Add Post</a>
-                </li>
-            </ul>
+            <div class="addpost-container">
+                <a class="btn-addpost rounded" href="<?= base_url() . "posts/add" ?>">Add Post</a>
+            </div>
             <div class="row row-cols-1 row-cols-md-3 g-4 collectionCard">
                 <?php if (count($posts) != 0 || count($posts) != NULL) : ?>
                     <?php foreach ($posts as $item) : ?>
                         <div class="col col-remove-margin">
                             <div class="card">
                                 <div class="boxImageContainer" style="background: url('<?= base_url('assets/uploads/posts/') . $item->post_photo ?>') center no-repeat; background-size: 100% 100%;">
-                                    <div class="d-flex justify-content-between p-3">
-                                        <a href="<?= base_url('posts/delete/'); ?><?= $item->post_id ?>">Delete Post</a>
-                                        <a href="<?= base_url('posts/edit/'); ?><?= $item->post_id ?>">Edit Post</a>
+                                    <div class="d-flex justify-content-between p-3 btn-card-top">
+                                        <a href="<?= base_url('posts/delete/'); ?><?= $item->post_id ?>">
+                                            <i class="material-icons card-top-button">delete</i>
+                                        </a>
+                                        <a href="<?= base_url('posts/edit/'); ?><?= $item->post_id ?>">
+                                            <i class="material-icons card-top-button">edit</i>
+                                        </a>
                                     </div>
                                 </div>
                                 <div class="card-body colcard-body d-flex align-items-center">
