21910bf7e13d5a0fc1a0bf48ee237447792ab403
PrynsTag <201812311@fit.edu.ph>
2021-07-20 04:34:37 +0800

    Fix update and insert bug in userinfo

--- application/models/Profile_model.php
+++ application/models/Profile_model.php
@@ -3,35 +3,37 @@
 
 class Profile_model extends CI_Model
 {
-	public function update_userdata($user_data, $user_id): bool
+	public function insert_userinfo($user_data, $user_id)
 	{
-		$user_db = [
-			"user_firstname" => $user_data["userinfo_firstname"],
-			"user_lastname" => $user_data["userinfo_lastname"]
-		];
-
-		$userinfo_db = [
-			"userinfo_firstname" => $user_data["userinfo_firstname"],
-			"userinfo_lastname" => $user_data["userinfo_lastname"],
-			"userinfo_bio" => $user_data["userinfo_bio"],
-			"userinfo_image" => $user_data["userinfo_image"]
-		];
-		$this->db->trans_start();
-		// User DB
-		$this->db->set($user_db);
-		$this->db->where('user_id', $user_id);
-		$this->db->update('user');
-
-		// UserInfo DB
-		$this->db->set($userinfo_db);
-		$this->db->where('user_id', $user_id);
-		$this->db->update('userinfo');
-
-		$this->db->trans_complete();
-		return $this->db->affected_rows() >= 0; //add your your code here
+		$this->db->where("user_id", $user_id);
+		$this->db->insert('userinfo', array_merge($user_data, ["user_id" => $user_id]));
+		return $this->db->insert_id();
 	}
 
-	public function update_password($user_data, $user_id)
+	public function update_userdata($user_data, $user_id): bool
+	{
+		$firstname = $user_data["userinfo_firstname"];
+		$lastname = $user_data["userinfo_lastname"];
+		$bio = $user_data["userinfo_bio"];
+		$image = $user_data["userinfo_image"];
+
+		$this->db->trans_start();
+		$this->db->query("UPDATE user 
+							SET user_firstname = '$firstname', 
+							    user_lastname = '$lastname' 
+							WHERE user_id = '$user_id'");
+		$this->db->query("UPDATE userinfo 
+							SET userinfo_firstname = '$firstname', 
+							    userinfo_lastname = '$lastname', 
+							    userinfo_bio = '$bio', 
+							    userinfo_image = '$image' 
+							WHERE userinfo_id = '$user_id'");
+		$this->db->trans_complete();
+
+		return $this->db->trans_status();
+	}
+
+	public function update_password($user_data, $user_id): bool
 	{
 		$this->db->set($user_data);
 		$this->db->where('user_id', $user_id);
@@ -43,6 +45,12 @@ class Profile_model extends CI_Model
 	{
 		$this->db->where("user_id", $user_id);
 		return $this->db->get("user")->result();
+	}
+
+	public function get_userinfo($user_id)
+	{
+		$this->db->where("user_id", $user_id);
+		return $this->db->get("userinfo")->result();
 	}
 
 	public function join_userdata($user_id)
