bfd906894e5309242f01fc03e2b7e7f420560e77
CWGGa <201812027@fit.edu.ph>
2021-07-20 01:45:06 +0800

    Added about page and design

--- application/controllers/About.php
+++ application/controllers/About.php
@@ -7,5 +7,13 @@ class About extends CI_Controller
         if ($this->session->userdata('user_info') == NULL) {
             redirect('login');
         }
+
+        $view_data = [
+            'header_title' => 'About - TechStra',
+            'main_view' => 'about_view',
+            'nav_icon' => 'astra-tech.jpg'
+        ];
+
+        $this->load->view('templates/home', $view_data);
     }
 }
---
+++ application/views/about_view.php
@@ -0,0 +1,47 @@
+<div class="aboutBody">
+    <h1 class="about-title">Our Team</h1>
+    <div class="container mx-auto mt-4">
+        <div class="row">
+            <div class="col-md-3 about-page">
+                <div class="card">
+                    <img src="<?= base_url("assets/images/team/pexels-min-an-654693.jpg"); ?>" class="card-img-top" alt="user-image">
+                    <div class="card-body">
+                        <h5 class="card-title about-page">User Name</h5>
+                        <h6 class="card-subtitle about-page mb-2 text-muted">User Role</h6>
+                        <p class="card-text about-page">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam nesciunt odio voluptas. Repellendus praesentium eos impedit est sit tempore alias.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card">
+                    <img src="<?= base_url("assets/images/team/pexels-min-an-654693.jpg"); ?>" class="card-img-top" alt="user-image">
+                    <div class="card-body">
+                        <h5 class="card-title about-page">User Name</h5>
+                        <h6 class="card-subtitle about-page mb-2 text-muted">User Role</h6>
+                        <p class="card-text about-page">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam nesciunt odio voluptas. Repellendus praesentium eos impedit est sit tempore alias.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card">
+                    <img src="<?= base_url("assets/images/team/pexels-min-an-654693.jpg"); ?>" class="card-img-top" alt="user-image">
+                    <div class="card-body">
+                        <h5 class="card-title about-page">User Name</h5>
+                        <h6 class="card-subtitle about-page mb-2 text-muted">User Role</h6>
+                        <p class="card-text about-page">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam nesciunt odio voluptas. Repellendus praesentium eos impedit est sit tempore alias.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card">
+                    <img src="<?= base_url("assets/images/team/pexels-min-an-654693.jpg"); ?>" class="card-img-top" alt="user-image">
+                    <div class="card-body">
+                        <h5 class="card-title about-page">User Name</h5>
+                        <h6 class="card-subtitle about-page mb-2 text-muted">User Role</h6>
+                        <p class="card-text about-page">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam nesciunt odio voluptas. Repellendus praesentium eos impedit est sit tempore alias.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
