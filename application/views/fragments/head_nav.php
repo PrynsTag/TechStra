fb4c7a46ee798270ad448a9a498491e1c749adbc
PrynsTag <201812311@fit.edu.ph>
2021-07-22 18:40:07 +0800

    Fix Profile Image not showing

--- application/views/fragments/head_nav.php
+++ application/views/fragments/head_nav.php
@@ -1,12 +1,8 @@
-<?php
-$session_data = $this->session->userdata('user_info');
-$userimage = $session_data['userimage'] == NULL || $session_data == '' ? 'default-image.png' : $session_data['userimage'];
-?>
-
 <nav class="navbar navbar-expand-lg head-nav">
 	<div class="container-fluid">
 		<a class="navbar-brand" href="<?= base_url("home") ?>">TechStra</a>
-		<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+		<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01"
+						aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
 			<i class="material-icons menu-icon">menu</i>
 		</button>
 
@@ -22,12 +18,19 @@ $userimage = $session_data['userimage'] == NULL || $session_data == '' ? 'defaul
 					<a class="nav-link <?= $this->uri->segment(1) === 'posts' ? 'active' : '' ?>" href="<?= base_url("about") ?>">About</a>
 				</li>
 				<li class="nav-item dropdown">
-					<a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
-						<img class="rounded-circle" src="<?= base_url("assets/uploads/user_profile/" . $userimage) ?>" alt="Profile-Image" height="42" width="42">
+					<a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true"
+						 aria-expanded="false">
+						<img src='<?php
+						if (isset($userinfo_image)) {
+							echo base_url("assets/uploads/user_profile/" . $userinfo_image);
+						} else {
+							echo base_url("assets/uploads/user_profile/default-image.png");
+						}
+						?>' class="rounded-circle" height="42" width="42" alt="User-Profile">
 					</a>
 					<div class="dropdown-menu" style="min-width: inherit">
 						<a class="dropdown-item" href="<?= base_url("profile/user") ?>">Profile</a>
-						<a class="dropdown-item" href="#">Setting</a>
+						<a class="dropdown-item" href="<?= base_url("profile/edit_profile") ?>">Setting</a>
 						<div class="dropdown-divider"></div>
 						<a class="dropdown-item" href="<?= base_url("logout") ?>">Logout</a>
 					</div>
