cdb62e379afd0e720aea439e50785ea675f51fe9
Prince Carl Velasco <github@princevelasco.com>
2023-01-09 19:59:17 +0800

    refactor: Include developers of project

--- application/views/about_view.php
+++ application/views/about_view.php
@@ -2,43 +2,21 @@
     <h1 class="about-title">Our Team</h1>
     <div class="container mx-auto mt-4">
         <div class="row">
-            <div class="col-md-3 about-page">
+            <div class="col-md-6 about-page">
                 <div class="card">
-                    <img src="<?= base_url("assets/images/team/pexels-min-an-654693.jpg"); ?>" class="card-img-top" alt="user-image">
+                    <img src="<?= base_url("assets/images/team/JC.webp"); ?>" class="card-img-top" alt="user-image">
                     <div class="card-body">
-                        <h5 class="card-title about-page">User Name</h5>
-                        <h6 class="card-subtitle about-page mb-2 text-muted">User Role</h6>
-                        <p class="card-text about-page">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam nesciunt odio voluptas. Repellendus praesentium eos impedit est sit tempore alias.</p>
+                        <h5 class="card-title about-page">John Christian Garon</h5>
+                        <h6 class="card-subtitle about-page mb-2 text-muted">Developer</h6>
                     </div>
                 </div>
             </div>
-            <div class="col-md-3">
+            <div class="col-md-6">
                 <div class="card">
-                    <img src="<?= base_url("assets/images/team/pexels-min-an-654693.jpg"); ?>" class="card-img-top" alt="user-image">
+                    <img src="<?= base_url("assets/images/team/Prince.webp"); ?>" class="card-img-top" alt="user-image">
                     <div class="card-body">
-                        <h5 class="card-title about-page">User Name</h5>
-                        <h6 class="card-subtitle about-page mb-2 text-muted">User Role</h6>
-                        <p class="card-text about-page">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam nesciunt odio voluptas. Repellendus praesentium eos impedit est sit tempore alias.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-3">
-                <div class="card">
-                    <img src="<?= base_url("assets/images/team/pexels-min-an-654693.jpg"); ?>" class="card-img-top" alt="user-image">
-                    <div class="card-body">
-                        <h5 class="card-title about-page">User Name</h5>
-                        <h6 class="card-subtitle about-page mb-2 text-muted">User Role</h6>
-                        <p class="card-text about-page">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam nesciunt odio voluptas. Repellendus praesentium eos impedit est sit tempore alias.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-3">
-                <div class="card">
-                    <img src="<?= base_url("assets/images/team/pexels-min-an-654693.jpg"); ?>" class="card-img-top" alt="user-image">
-                    <div class="card-body">
-                        <h5 class="card-title about-page">User Name</h5>
-                        <h6 class="card-subtitle about-page mb-2 text-muted">User Role</h6>
-                        <p class="card-text about-page">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam nesciunt odio voluptas. Repellendus praesentium eos impedit est sit tempore alias.</p>
+                        <h5 class="card-title about-page">Prince Carl Velasco</h5>
+                        <h6 class="card-subtitle about-page mb-2 text-muted">Project Manager/Lead Developer</h6>
                     </div>
                 </div>
             </div>
